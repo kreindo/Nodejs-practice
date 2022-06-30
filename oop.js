5989172ad4d392f0e3c2e0f9f014d304dc308be5
ahmadsan <ahmadsan4p@gmail.com>
2022-06-30 09:23:39 +0800

    oop

---
+++ oop.js
@@ -0,0 +1,29 @@
+class Creature {
+    constructor(id, isAlive) {
+        this.id = id;
+        this.isAlive = isAlive;
+        this.crData = [];
+    }
+    showId = () => console.log(`creature id ${this.id}`);
+    detectLife = () => console.log(`creature ${this.isAlive ? 'is alive' : 'is dead'}`)
+}
+
+class HostileCreatures extends Creature{
+    constructor(id, ai, health, stat){
+        super(id);
+        this.ai = ai;
+        this.health = health;
+        this.stats = stat;
+
+    }
+    defineNewHostileCreature = (creatureName) => console.log(`${creatureName} is created with id of ${this.id || id}, health set to = ${this.health} and stats is set to ${this.stats}`);
+
+}
+
+const lizard = new HostileCreatures(1, 'hostile', 1000, {
+    str: 10,
+    dex: 8,
+    res: 15
+})
+
+lizard.defineNewHostileCreature('Red boi')
